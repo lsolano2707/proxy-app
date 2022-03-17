37300c2c72a90e42c78c2234ce521cdaf949f9f2
Leonardo Solano <lsolano@wfscorp.com>
2022-03-16 23:03:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const port = process.env.APP_PORT || 3000;
+
+app.get('/', (req, res) => {
+  if (!req.query.url) {
+    return res.status(400).send('url is required');
+  }
+
+  axios.get(req.query.url).then(resp => {
+    console.log('resp', resp.data);
+    res.send(resp.data)
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
